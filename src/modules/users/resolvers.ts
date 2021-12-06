06d01cc39d5e67d1391ffbf4edab772eb38ec503
Pedro-V8 <pedrophdv@outlook.com>
2021-12-06 10:50:45 -0300

    Implementing Service registration

--- src/modules/users/resolvers.ts
+++ src/modules/users/resolvers.ts
@@ -1,38 +1,30 @@
 import User from '../../models/User';
 import Post from '../../models/Posts';
+import { register } from '../../services/AuthenticateService';
+
 function deletaPost(arr: any){
     arr.forEach((e: any) => {
         async () => await Post.deleteOne(e)
     });
 }
-let mArr: Array<any> = [];
-let post: any;
-const teste = async (id: any) => {
-    await Post.findOne(id).then(async(ps) => await mArr.push(ps)) 
-    //console.log(mArr)
-}
-function retornaPosts(arr: any){
-    arr.map((el: any) => { 
-        teste(el) 
-        //console.log((el))
-    })
-}
+
 
 export default {
     Query: {
         users: async () => await User.find(),
         user: async (_: any, {id}: any) =>{
             const user =  await User.findById(id).populate('posts');
-            // retornaPosts(user.posts);
-            // user.posts.push(mArr);
-            // console.log(user.posts);
 
             return user;
         },
     },
     Mutation: {
-        createUser: async (_:any , {data}:any) => await User.create(data),
+        createUser: async (_:any , {data}:any) => {
+            const user: any = register(data);
+            console.log(user);
+            return user;
+        },
         updateUser: async (_: any , {id , data }: any) => await User.findOneAndUpdate(id , data),
-        deleteUser: async (_: any , {id }: any ) => await User.deleteOne(id),
+        deleteUser: async (_: any , {id }: any ) => await User.findByIdAndDelete(id),
     },
 };
--- src/services/AuthenticateService.ts
+++ src/services/AuthenticateService.ts
@@ -1,11 +1,17 @@
 import User from '../models/User';
 import { sign } from 'jsonwebtoken';
 
+export const register = async(obj: any) => {
+    const user = await User.create(obj);
+    console.log(user)
+    return { user };
+};
 
 export const signIn = async(obj: any) => {
-    const user = await User.findOne({ email: obj});
-    console.log(user)
+    const user = await User.findOne({ email: obj}).select('+password');
+
     if(user){
+        console.log(user)
         return { user };
     };
 }
